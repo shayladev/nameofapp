ece117c9da036f57be9016f018e5020175de25fb
shayladev <shaydev@gmail.com>
2019-01-08 21:48:46 -0500

    4.3 Orders manual scaffold

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,19 @@
+class OrdersController < ApplicationController
+
+  # GET /orders
+  # GET /orders.json
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
   get 'simple_pages/about'
   get 'simple_pages/contact'
-  root 'simple_pages#index'
+  root 'simple_pages#landing_page'
   get 'simple_pages/index'
   get 'products/index'
+  get 'orders/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
