5d96972464305a6990b2be29f3d6b507828ad540
shayladev <shaydev@gmail.com>
2019-01-10 20:50:10 -0500

    4.5 adding a carousel to landing page view

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -4,6 +4,8 @@ class SimplePagesController < ApplicationController
 
   def landing_page
     @featured_product = Product.first
+    @products = Product.limit(3)
+    render layout: "landing_page"
   end
 
 end
