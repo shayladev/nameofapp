632f0478cda740c12185698bcf05e006e785d003
shayladev <shaydev@gmail.com>
2019-01-14 17:08:55 -0500

    4.5 views updated carousel and seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(name:"seed-generated product", description: "I also created this product without using the HTML form!")
+PLACEHOLDER_IMAGE = "https://via.placeholder.com/600x400"
+LOREM = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+COLORS = ["red", "green", "blue", "pink", "purple", "orange", "black", "white"]
+
+# How many seeded samples do you want?
+PRODUCT_SEED_COUNT = 3
+
+def random_price
+  rand(1..10)*10
+end
+
+def random_color
+  COLORS.sample
+end
+
+1.upto(PRODUCT_SEED_COUNT) do |i|
+  Product.create(name:"seed-generated product ##{i}", description: LOREM, image_url: PLACEHOLDER_IMAGE, colour: random_color, price: random_price)
+end
