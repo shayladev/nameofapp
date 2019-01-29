3e1b4da6fdf7e9ba2cf0d3b74b5155601dc7126c
shayladev <shaydev@gmail.com>
2019-01-29 18:01:46 -0500

    5.2 Forms

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ApplicationRecord
+  #automatically created a Product model associated to a Products table
+
+  def self.search(search_term)
+    if Rails.env == "development"
+      Product.where("name LIKE ?", "%#{search_term}%")
+    else
+      Product.where("name ilike ?", "%#{search_term}%")
+    end
+  end
+
 end
