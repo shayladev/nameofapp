0fba5c6da2356a0d5267a5d47e3ecf2a71257d49
shayladev <shaydev@gmail.com>
2019-01-30 15:06:33 -0500

    5.3 Sending Emails

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -8,4 +8,13 @@ class SimplePagesController < ApplicationController
     render layout: "landing_page"
   end
 
+  # POST simple_pages/thank_you
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver_now
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   root 'simple_pages#landing_page'
   get 'simple_pages/index'
   get 'orders/index'
+  post 'simple_pages/thank_you'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
