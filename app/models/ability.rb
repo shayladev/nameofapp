682d76ccf96a1b62c468e1cc7af050cab31f5fd9
shayladev <shaydev@gmail.com>
2019-02-15 09:32:50 -0500

    5.10 CanCanCan fix

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -31,7 +31,7 @@ class Ability
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
       if user.admin?
-        can: :destroy, :Comment
+        can :destroy, :Comment
       end
   end
 end
