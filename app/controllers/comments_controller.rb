c24400e8b6f35fea611eec0c53ab60ba54a9f476
shayladev <shaydev@gmail.com>
2019-03-18 12:06:44 -0400

    6.10 AJAX

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,20 @@ class CommentsController < ApplicationController
     @product = Product.find(params[:product_id])
     @comment = @product.comments.new(comment_params)
     @comment.user = current_user
-    @comment.save
-    redirect_to product_path(@product)
+    # @comment.save
+    # redirect_to product_path(@product)
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @product, notice: 'Review was created successfully'}
+        format.json { render :show, status: :created, location: @product }
+        format.js
+      else
+        format.html { redirect_to @product, alert: 'Review was not saved successfully'}
+        format.json { render json: @comment.errors, status: :unprocessable_entity}
+      end
+    end
+
   end
 
   def destroy
@@ -14,15 +26,7 @@ class CommentsController < ApplicationController
     redirect_to product
   end
 
-  respond_to do |format|
-    if @comment.save
-      format.html { redirect_to @product, notice: 'Review was created successfully'}
-      format.json { render :show, status: :created, location: @product }
-    else
-      format.html { redirect_to @product, alert: 'Review was not saved successfully'}
-      format.json { render json: @comment.errors, status: :unprocessable_entity}
-    end
-  end
+
 
   private
 
